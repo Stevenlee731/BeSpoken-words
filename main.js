af4635d3a2de1e37034d6b82321a46b0dd28aac7
Stevenlee731 <Stevenlee731@gmail.com>
2017-03-14 16:51:41 -0700

    created DOM elements

--- main.js
+++ main.js
@@ -1,49 +1,98 @@
+/*
+<div class="row">
+  <div class="col-sm-4 columns">
+    <div class="card">
+      <img class="card-img-top img-fluid" src="images/10057683GLargeSoftRuledNotebook_600.jpg" alt="Card image cap">
+      <div class="card-block">
+        <h4 class="card-title">Moleskine</h4>
+        <p class="card-text">A blending of tones, fabric and texture, this notebook is where touch and colors meet.</p>
+        <p class="card-text">Learn More</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4 columns">
+    <div class="card">
+      <img class="card-img-top img-fluid" src="images/10067179gMetallicNotebook_600.jpg" alt="Card image cap">
+      <div class="card-block">
+        <h4 class="card-title">Pippin</h4>
+        <p class="card-text">Think of our Pippin Medium Soft Cover Notebook as your trusty right (or left!) - hand-man.</p>
+        <p class="card-text">Learn More</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4 columns">
+    <div class="card">
+      <img class="card-img-top img-fluid" src="images/10067053ArtisanJournalCalhoun_600.jpg" alt="Card image cap">
+      <div class="card-block">
+        <h4 class="card-title">Calhoun</h4>
+        <p class="card-text">Russel &amp; Hazel, updates the classic coptic journal with a contemporary cover.</p>
+        <p class="card-text">Learn More</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a>
+      </div>
+    </div>
+  </div>
+</div>
+*/
+
 var items = [
   {
     name: 'Moleskine',
-    description: 'A blending of tones, fabric and texture, this notebook collection is where touch and colors meet. The Jacquard woven fabric cover comes in a sophisticated, new and naturally inspired color palette with bookmark ribbons and elastic closures in striking complimentary colors. Engage your senses as you weave your own story on the blank pages.',
-    button: 'Add to Cart'
+    description: 'A blending of tones, fabric and texture, this notebook is where touch and colors meet.',
+    learnMore: 'Learn More',
+    button: 'Add to Cart',
+    image: 'images/10057683GLargeSoftRuledNotebook_600.jpg'
   },
   {
     name: 'Poppin',
-    description: 'Think of our Medium Soft Cover Notebook as your trusty right(or left!)-hand-man. Always there when you need it, this purse-or-briefcase pal lives to hold meeting notes or keep a lid on secret plans. With a handy back pocket and totally luxe cover, it\'s the best-looking and hardest-working assistant money can buy.',
-    button: 'Add to Cart'
+    description: 'Think of our Pippin Medium Soft Cover Notebook as your trusty right (or left!) - hand-man.',
+    learnMore: 'Learn More',
+    button: 'Add to Cart',
+    image: 'images/10067053ArtisanJournalCalhoun_600.jpg'
   },
   {
     name: 'Calhoun',
-    description: 'Russel & Hazel updates the classic coptic journal with a contemporary cover - Dry-Erase features and reinforced corners for a versatile notebook.',
-    button: 'Add to Cart'
+    description: 'Russel & Hazel, updates the classic coptic journal with a contemporary cover.',
+    button: 'Add to Cart',
+    learnMore: 'Learn More',
+    image: 'images/10067179gMetallicNotebook_600.jpg'
   }
 ]
 
 items.forEach(function(item) {
-  var $row = document.createElement('div')
   var $col = document.createElement('div')
-  var $item = document.createElement('div')
-  var $itemName = document.createElement('div')
-  var $itemDescription = document.createElement('div')
+  var $card = document.createElement('div')
   var $image = document.createElement('img')
-  var $button = document.createElement('button')
+  var $cardBlock = document.createElement('div')
+  var $itemName = document.createElement('h4')
+  var $itemDescription = document.createElement('p')
+  var $learnMore = document.createElement('p')
+  var $button = document.createElement('a')
 
-  $row.appendChild($col)
-  $col.appendChild($item)
-  $item.appendChild($itemName)
-  $item.appendChild($itemDescription)
-  $item.appendChild($image)
-  $item.appendChild($button)
+  $col.appendChild($card)
+  $card.appendChild($image)
+  $card.appendChild($cardBlock)
+  $cardBlock.appendChild($itemName)
+  $cardBlock.appendChild($itemDescription)
+  $cardBlock.appendChild($learnMore)
+  $cardBlock.appendChild($button)
 
-  $row.classList.add('row')
-  $col.classList.add('col-xs-12')
-  $item.classList.add('panel')
-  $item.classList.add('panel-default')
-  $itemName.classList.add('panel-body')
-  $itemDescription.classList.add('panel-body')
-  $image.setAttribute('src', 'images/10057683GLargeSoftRuledNotebook_600.jpg')
+  $col.classList.add('col-sm-4', 'columns')
+  $card.classList.add('card')
+  $image.classList.add('card-img-top', 'img-fluid')
+  $cardBlock.classList.add('card-block')
+  $itemName.classList.add('card-title')
+  $itemDescription.classList.add('card-text')
+  $learnMore.classList.add('card-text')
+  $button.classList.add('btn', 'btn-primary')
+  $image.setAttribute('src', item.image)
 
   $itemName.textContent = item.name
   $itemDescription.textContent = item.description
+  $learnMore.textContent = item.learnMore
   $button.textContent = item.button
 
   var $items = document.querySelector('#items')
-  $items.appendChild($row)
+  $items.appendChild($col)
 })
