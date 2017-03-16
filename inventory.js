7c5de90fcd880f5c110fbd310763bf3a00d17dc9
Stevenlee731 <Stevenlee731@gmail.com>
2017-03-15 17:08:19 -0700

    Created Details list for DOM and made it hidden

---
+++ inventory.js
@@ -0,0 +1,64 @@
+var items = [
+  {
+    id: 1,
+    name: 'Moleskine',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    addToCart: 'www.google.com',
+    learnMore: 'Learn More',
+    button: 'Add to Cart',
+    image: 'images/10057683GLargeSoftRuledNotebook_600.jpg'
+  },
+  {
+    id: 2,
+    name: 'Poppin',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    addToCart: 'www.google.com',
+    learnMore: 'Learn More',
+    button: 'Add to Cart',
+    image: 'images/10068166MediumPoppingSoftCoverNotebo.jpg'
+  },
+  {
+    id: 3,
+    name: 'Baron Fig',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    button: 'Add to Cart',
+    addToCart: 'www.google.com',
+    learnMore: 'Learn More',
+    link: 'https://google.com',
+    image: 'images/Confidant_Perspective_v03.jpg'
+  }
+]
+
+var itemsDetail = [
+  {
+    id: 1,
+    name: 'Moleskine',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    addToCart: 'www.google.com',
+    learnMore: 'Learn More',
+    button: 'Add to Cart',
+    cost: '$13.99',
+    image: 'images/Moleskin-detail.jpg'
+  },
+  {
+    id: 2,
+    name: 'Poppin',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    addToCart: 'www.google.com',
+    learnMore: 'Learn More',
+    button: 'Add to Cart',
+    cost: '$15.99',
+    image: 'images/01 Header.jpg'
+  },
+  {
+    id: 3,
+    name: 'Baron Fig',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    button: 'Add to Cart',
+    addToCart: 'www.google.com',
+    learnMore: 'Learn More',
+    link: 'https://google.com',
+    cost: '$18.99',
+    image: 'images/ks_squire_photos-wnotebooks_02_2048x2048.jpg'
+  }
+]
--- main.js
+++ main.js
@@ -38,38 +38,6 @@
 
 // navigation
 
-var navItems = [
-  {}
-]
-
-var items = [
-  {
-    id: 1,
-    name: 'Moleskine',
-    description: 'A blending of tones, fabric and texture, this notebook is where touch and colors meet.',
-    learnMore: 'Learn More',
-    button: 'Add to Cart',
-    image: 'images/10057683GLargeSoftRuledNotebook_600.jpg'
-  },
-  {
-    id: 2,
-    name: 'Poppin',
-    description: 'Think of our Pippin Medium Soft Cover Notebook as your trusty right (or left!) - hand-man.',
-    learnMore: 'Learn More',
-    button: 'Add to Cart',
-    image: 'images/10067053ArtisanJournalCalhoun_600.jpg'
-  },
-  {
-    id: 3,
-    name: 'Calhoun',
-    description: 'Russel & Hazel, updates the classic coptic journal with a contemporary cover.',
-    button: 'Add to Cart',
-    learnMore: 'Learn More',
-    link: 'https://google.com',
-    image: 'images/10067179gMetallicNotebook_600.jpg'
-  }
-]
-
 
 // Main Page
 function renderItem(item) {
@@ -100,6 +68,7 @@ function renderItem(item) {
   $itemDescription.classList.add('card-text')
   $learnMore.classList.add('card-text')
   $button.classList.add('btn', 'btn-primary')
+  $button.setAttribute('href', item.addToCart)
   $image.setAttribute('src', item.image)
   $learnMore.setAttribute('href', item.link)
 
@@ -118,7 +87,83 @@ items.forEach(function (item) {
   $items.appendChild($item)
 })
 
-// Detailed View
+/* Detailed View
+<div class="col-sm-12">
+  <div class="card">
+    <img class="card-img-top img-fluid" src="images/Moleskin-detail.jpg" alt="Card image cap">
+    <div class="card-block">
+      <h3 class="card-title">Moleskine Notebook</h3>
+      <p class="card-text">It all started with a notebook. Distinguishing marks: a hard cover with rounded corners and an elastic closure. On the first page an "in case of loss" notice with space to jot down a reward for the finder. Attached to the back cover an expandable pocket for loose papers and notes.</p>
+      <a href="#" class="btn btn-primary">Add to Cart</a>
+    </div>
+  </div>
+</div>
+<div class="col-sm-12">
+  <div class="card">
+    <img class="card-img-top img-fluid" src="images/01 Header.jpg" alt="Card image cap">
+    <div class="card-block">
+      <h3 class="card-title">Poppin Notebook</h3>
+      <p class="card-text">Think of our Medium Soft Cover Notebook as your trusty right(or left!)-hand-man. Always there when you need it, this purse-or-briefcase pal lives to hold meeting notes or keep a lid on secret plans. With a handy back pocket and totally luxe cover, it's the best-looking and hardest-working assistant money can buy.</p>
+      <a href="#" class="btn btn-primary">Add to Cart</a>
+    </div>
+  </div>
+</div>
+<div class="col-sm-12">
+  <div class="card">
+    <img class="card-img-top img-fluid" src="images/ks_squire_photos-wnotebooks_02_2048x2048.jpg" alt="Card image cap">
+    <div class="card-block">
+      <h3 class="card-title">Baron Fig Notebook</h3>
+      <p class="card-text">Designed to be used and abused, it's the perfect counterpart to the Confidant. The name honors the very community whose feedback inspired its creation. Vanguard—adventurer and leader. The tool for pioneers of the mind.</p>
+      <a href="#" class="btn btn-primary">Add to Cart</a>
+    </div>
+  </div>
+</div>
+*/
+
+function renderDetails(item) {
+  var $col = document.createElement('div')
+  var $card = document.createElement('div')
+  var $image = document.createElement('img')
+  var $cardBlock = document.createElement('div')
+  var $itemName = document.createElement('h3')
+  var $itemDescription = document.createElement('p')
+  var $itemCost = document.createElement('p')
+  var $button = document.createElement('a')
+
+  $col.appendChild($card)
+  $card.appendChild($image)
+  $card.appendChild($cardBlock)
+  $cardBlock.appendChild($itemName)
+  $cardBlock.appendChild($itemDescription)
+  $cardBlock.appendChild($itemCost)
+  $cardBlock.appendChild($button)
+
+  $col.classList.add('col-sm-12')
+  $card.classList.add('card')
+  $image.classList.add('card-img-top', 'img-fluid')
+  $cardBlock.classList.add('card-block')
+  $itemName.classList.add('card-title')
+  $itemDescription.classList.add('card-text')
+  $itemCost.classList.add('card-text')
+  $button.classList.add('btn', 'btn-primary')
+  $button.setAttribute('href', item.addToCart)
+  $image.setAttribute('src', item.image)
+
+  $itemName.textContent = item.name
+  $itemDescription.textContent = item.description
+  $itemCost.textContent = item.cost
+  $button.textContent = item.button
+
+
+  return $col
+}
+
+var $itemsDetail = document.querySelector('#details')
+
+itemsDetail.forEach(function (item) {
+  var $item = renderDetails(item)
+  $itemsDetail.appendChild($item)
+})
 
 /*
 document.body.addEventListener('click', function(event) {
